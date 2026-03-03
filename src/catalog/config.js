d9c8c9a9d539d7aa0b93e2dbbb1036bb5bf97a03
Ngoc Luong <ngoclb@carmd.com>
2026-03-03 14:30:39 +0700

    fix: use relative base url

--- src/catalog/config.js
+++ src/catalog/config.js
@@ -1,16 +1,16 @@
 const DEFAULT_CONFIG = {
   pageSize: 5,
-  catalogCsv: "/catalog.csv",
-  changelogFolder: "/releases",
+  catalogCsv: "catalog.csv",
+  changelogFolder: "releases",
   changelogFile: "CHANGELOG.md",
 
   getAssetIconUrl(asset) {
-    if (asset.includes(".zip")) return "/assets/svg/package.svg";
+    if (asset.includes(".zip")) return "assets/svg/package.svg";
     if (asset.includes(".ipa") || asset.includes(".plist"))
-      return "/assets/svg/ios.svg";
+      return "assets/svg/ios.svg";
     if (asset.includes(".apk") || asset.includes(".aab"))
-      return "/assets/svg/android.svg";
-    return "/assets/svg/unknown.svg";
+      return "assets/svg/android.svg";
+    return "assets/svg/unknown.svg";
   },
 
   getAssetDownloadUrl(version, asset) {
